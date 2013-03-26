2a99a0682397b8c4cb456148a643c8259fb35c68
Matias Domingues <matiasdomingues@gmail.com>
2013-03-26 17:36:55 -0300

    Add Server - Client IPC pipe basic interaction, Set protocol, Set waiting Queues

---
+++ message.h
@@ -0,0 +1,19 @@
+typedef struct _message{
+	char protocol[9]; // "TPSO 0.1"
+	char method[12];
+	char resource[12];
+	time_t time; 
+	int referer;
+	char body[30];
+}Message;
+
+typedef struct _task{
+	Message* msg;
+	struct _task * next;
+}Task;
+
+typedef struct _client{
+	int pid;
+	FILE* fd;
+	struct _client * next;
+}Client;

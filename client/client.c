d7582a34b47b7ce5c416efa437dec84000533ece
Matias Domingues <matiasdomingues@gmail.com>
2013-04-22 17:56:29 -0300

    Client using transporter

--- client/client.c
+++ client/client.c
@@ -11,31 +11,22 @@
 
 #include "../includes/message.h"
 #include "../includes/mail.h"
+#include "../includes/transporter.h"
+
 #include <time.h>
 
-Message* prepareMessage();
-Message* fillMessageData(Message* msg, char* method, char* resource, char* body);
+Message* fillMessageData(char* resource, char* method, char* body);
 void dispatchEvent(Message* msg);
-void listenForConnection();
-char* createPipe();
-void sendData(char* resource, char* method, char* body);
 void setLogin(char* name);
 void writeEmail();
 void grabNewEmails();
 void readConsole();
 
 int status = 0;
-int receiver;
-int sender;
 char username[15];
-fd_set active_fd_set;
 
-Message* prepareMessage() {
-	Message* info = (Message*) malloc(sizeof(Message));
-	return info;
-}
-
-Message* fillMessageData(Message* msg, char* method, char* resource, char* body) {
+Message* fillMessageData(char* resource, char* method, char* body){
+	Message* msg = malloc(sizeof(Message)); 
 	strcpy(msg->protocol, "TPSO 0.12");
 	strcpy(msg->method, method);
 	strcpy(msg->resource, resource);
@@ -46,18 +37,19 @@ Message* fillMessageData(Message* msg, char* method, char* resource, char* body)
 
 }
 
+void getResponce(){
+	Message* msg = listenMessage(getpid(), sizeof(Message));
+	if(msg != NULL){
+		dispatchEvent(msg);
+	}
+}
+
 void recieveEmails(int num){
 	int i, error;
-	mail* msg = malloc(sizeof(mail));
+	mail* msg;
 	for(i=0; i<num; i++){
-		if (select(FD_SETSIZE, &active_fd_set, NULL, NULL, NULL ) < 0) {
-			perror("select");
-		}
-		error = read(receiver, msg, sizeof(mail));
-		if (error == -1) {
-			printf("Error on mail\n");
-			//perror("read");
-		} else if (error >= 1) {
+		msg = listenMessage(getpid(), sizeof(mail));
+		if(msg != NULL){
 			printf("%d: From: %s, Subject: %s Time: %s", i+1, msg->from, msg->header, ctime(&(msg->senttime)));
 			printf("Message:%s\n", msg->body);
 			if(msg->attachments[0] != '0'){
@@ -110,46 +102,6 @@ void dispatchEvent(Message* msg) {
 	}
 }
 
-void listenForConnection() {
-	int error = 0;
-	Message* msg = malloc(sizeof(Message));
-	if (select(FD_SETSIZE, &active_fd_set, NULL, NULL, NULL ) < 0) {
-		perror("select");
-	}
-	error = read(receiver, msg, sizeof(Message));
-	if (error == -1) {
-		free(msg);
-		//perror("read");
-	} else if (error >= 1) {
-		dispatchEvent(msg);
-	}
-
-}
-
-char* createPipe() {
-	char pid[7];
-	char* route = malloc(sizeof(char) * 16);
-	sprintf(pid, "%d", getpid());
-	strcpy(route, "/tmp/serv.");
-	strcat(route, pid);
-	printf("%d", mkfifo(route, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH));
-	return route;
-}
-
-void sendData(char* resource, char* method, char* body) {
-	int status;
-	Message* msg = prepareMessage();
-	msg = fillMessageData(msg, method, resource, body);
-	while ((status = write(sender, msg, sizeof(Message))) == 0) {
-		printf("paso2\n");
-	}
-	if (status == -1) {
-		printf("ERROR ON PIPE write\n");
-	}
-	free(msg);
-	listenForConnection();
-}
-
 void setLogin(char* name) {
 	strcpy(username, name);
 	status = 1;
@@ -178,18 +130,21 @@ void writeEmail() {
 	}
 	elem->read = 0;
 	elem->senttime = sendtime;
-	sendData("mail","send", "1");
-	printf("wrote: %d\n", (int)write(sender, elem, sizeof(mail)));
-	free(elem);
-	listenForConnection();
+	sendData(0, fillMessageData("mail","send", "1"), sizeof(Message));
+	getResponce();
+	sendData(0, elem, sizeof(mail));
+	//free(elem);
+	getResponce();
 }
 
 void grabNewEmails() {
-	sendData("mail", "receive", username);
+	sendData(0, fillMessageData("mail", "receive", username), sizeof(Message));
+	getResponce();
 }
 
 void getFee(){
-	sendData("user", "fee", username);
+	sendData(0, fillMessageData("user", "fee", username), sizeof(Message));
+	getResponce();
 }
 
 void readConsole() {
@@ -204,13 +159,15 @@ void readConsole() {
 			printf(
 					"Ingrese Usuario y Contraseña\nSeparados por ',' de la forma Usuario,Contraseña\n");
 			scanf("%30s", result);
-			sendData("login", "login", result);
+			sendData(0, fillMessageData("login", "login", result), sizeof(Message));
+			getResponce();
 
 		} else if (a == 2) {
 			printf(
 					"Ingrese Nombre, Usuario y Contraseña\nSeparados por ',' de la forma\nNombre,Usuario,Contraseña\n");
 			scanf("%30s", result);
-			sendData("login", "register", result);
+			sendData(0, fillMessageData("login", "register", result), sizeof(Message));
+			getResponce();
 
 		}
 	} else if (status == 1) {
@@ -230,14 +187,10 @@ void readConsole() {
 }
 
 int main() {
-	char* route;
-	route = createPipe();
-	printf("%s\n", route);
-	sender = open("/tmp/serv.xxxxx", O_WRONLY | O_NONBLOCK);
-	receiver = open(route, O_RDONLY | O_NONBLOCK);
-	FD_ZERO(&active_fd_set);
-	FD_SET(receiver, &active_fd_set);
-	sendData("client", "register", "");
+	openClient(0);
+	createConnection(getpid());
+	sendData(0, fillMessageData("client", "register", ""), sizeof(Message));
+	getResponce();
 	while (1) {
 		readConsole();
 	}

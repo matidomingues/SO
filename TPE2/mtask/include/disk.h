b0497ace2597a1203b75ba2038bf2af2ae920a87
Federelli <federelli@gmail.com>
2013-06-17 03:06:45 -0300

    Debugging and minor fixes.

---
+++ TPE2/mtask/include/disk.h
@@ -0,0 +1,80 @@
+#ifndef DISK_H
+#define DISK_H
+
+#include "kernel.h"
+
+/* I/O Ports used by winchester disk controller. */
+#define ATA0 0x1f0
+#define ATA1 0x170
+
+#define WIN_REG0       0x0
+#define WIN_REG1       0x1 // Error
+#define WIN_REG2       0x2
+#define WIN_REG3       0x3
+#define WIN_REG4       0x4
+#define WIN_REG5       0x5
+#define WIN_REG6       0x6
+#define WIN_REG7       0x7 // Command|Status
+#define WIN_REG8       0x3f6
+
+/* Winchester disk controller command bytes. */
+#define WIN_IDENTIFY	0xEC
+#define MEDIA_STATUS	0xDA
+#define READ_DMA		0xC8
+#define WIN_RECALIBRATE	0x10	/* command for the drive to re-calibrate */
+#define WIN_READ        0x20	/* command for the drive to read */
+#define WIN_WRITE       0x30	/* command for the drive to write */
+#define WIN_SPECIFY     0x91	/* command for the controller to accept params */
+
+#define LBA_READ        WIN_READ
+#define LBA_WRITE       WIN_WRITE
+
+/* Parameters for the disk drive. */
+#define SECTOR_SIZE      512	/* physical sector size in bytes */
+
+/* Miscellaneous. */
+#define MAX_ERRORS         4	/* how often to try r/w before quitting */
+#define NR_DEVICES        10	/* maximum number of drives */
+#define MAX_WIN_RETRY  10000	/* max # times to try to output to WIN */
+#define PART_TABLE     0x1C6	/* IBM partition table starts here in sect 0 */
+#define DEV_PER_DRIVE      5	/* hd0 + hd1 + hd2 + hd3 + hd4 = 5 */
+
+typedef struct disk_cmd {
+	int ata;
+	int sector;
+	int offset;
+	int bytes;
+	char * buffer;
+} disk_cmd;
+
+/* Escribe en el disco ata, los bytes indicados por parametro desde msg al sector "sector" con el offset "offset".
+ * El offset comienza a partir del 0 y cada sector tiene 512 bytes. En caso que offset >= 512, se normaliza a 512 e incrementa el
+ * sector (offset / 512) veces.
+ */
+void ata_write(int ata, void* msg, int bytes, unsigned short sector, int offset);
+
+/* Lee del disco ata, los bytes indicados por parametro a msg desde el sector "sector" con el offset "offset".
+ * El offset comienza a partir del 0 y cada sector tiene 512 bytes. En caso que offset >= 512, se normaliza a 512 e incrementa el
+ * sector (offset / 512) veces.
+ */
+void ata_read(int ata, void* msg, int bytes, unsigned short sector, int offset);
+
+/* Devuelve el registro de estado del disco*/
+unsigned short ata_getStatusRegister(int ata);
+
+/* Identifica el disco*/
+void identifyDevice(int ata);
+
+/*Chequea los features que soporta el disco*/
+void ata_checkDrive(int ata);
+
+unsigned short getErrorRegister(int ata);
+
+/*
+ * Normaliza sector y offset de acuerdo al tamaño maximo del offset de cada sector.
+ * luego de esta funcion, 0 <= offset < 512
+ * sector = sector + offset / 512
+ */
+void ata_normalize(unsigned short* sector, int* offset);
+
+#endif
---
+++ TPE2/mtask/src/disk.c
@@ -0,0 +1,209 @@
+#include "../include/disk.h"
+
+#define BIT(i) (1 << (i))
+
+#define IS_REMOVABLE(D)    printk("%ses removible\n", (D & BIT(7)) ? "" : "No ")
+#define IS_ATA_DRIVE(D)    printk("%ses ATA\n", (D & BIT(15)) ? "No " : "")
+#define DMA_SUP(D)         printk("%ssoporta DMA\n", (D & BIT(8)) ? "" : "No ")
+#define LBA_SUP(D)         printk("%ssoporta LBA\n", (D & BIT(9)) ? "" : "No ")
+#define DMA_QUEUED_SUP(D)  printk("%ssoporta DMA QUEUED\n", (D & BIT(1)) ? "" : "No ")
+
+void sendComm(int ata, int rdwr, unsigned short sector);
+void _read(int ata, char * ans, unsigned short sector, int offset, int count);
+void _write(int ata, char * msg, int bytes, unsigned short sector, int offset);
+
+unsigned short getDataRegister(int ata);
+void writeDataToRegister(int ata, char upper, char lower);
+
+void translateBytes(char ans[], unsigned short sector);
+
+void ata_normalize(unsigned short* sector, int* offset) {
+	if (*offset >= SECTOR_SIZE) {
+		*sector += (*offset / SECTOR_SIZE);
+		*offset %= SECTOR_SIZE;
+	}
+}
+
+void ata_read(int ata, void* ans, int bytes, unsigned short sector, int offset) {
+	mt_cli();
+	if (ata != ATA0 && ata != ATA1) {
+		printk("No existe el disco %d - [%d, %d]\n", ata, sector, offset);
+		return;
+	}
+	while (bytes != 0) {
+		if (offset >= SECTOR_SIZE) {
+			sector += (offset / SECTOR_SIZE);
+			offset %= SECTOR_SIZE;
+		}
+		int size;
+		if (offset + bytes > SECTOR_SIZE) { // read remaining part from the sector
+			size = SECTOR_SIZE - offset;
+			_read(ata, ans, sector, offset, size);
+			sector++;
+			offset = 0;
+			bytes -= size;
+			ans += size;
+		} else { // The remaining msg fits in the actual sector
+			size = bytes;
+			_read(ata, ans, sector, offset, size);
+			offset += size;
+			bytes = 0;
+			ans += size;
+		}
+	}
+	mt_sti();
+}
+
+void _read(int ata, char * ans, unsigned short sector, int offset, int count) {
+	char tmp[SECTOR_SIZE];
+	sendComm(ata, LBA_READ, sector);
+	/* Read sector*/
+	int b;
+	unsigned short data;
+	for (b = 0; b < SECTOR_SIZE; b += 2) {
+		data = getDataRegister(ata);
+		translateBytes(tmp + b, data);
+	}
+	int i;
+	for (i = 0; i < count; i++) {
+		ans[i] = tmp[offset + i];
+	}
+}
+
+void translateBytes(char * ans, unsigned short databyte) {
+	ans[0] = databyte & 0xff;
+	ans[1] = (databyte >> 8) & 0xFF;
+}
+
+void ata_write(int ata, void * msg, int bytes, unsigned short sector,
+		int offset) {
+	mt_cli();
+	if (ata != ATA0 && ata != ATA1) {
+		printk("El disco no existe %d - [%d, %d]\n", ata, sector, offset);
+		return;
+	}
+	char* ans = (char*) msg;
+	while (bytes != 0) {
+		if (offset >= SECTOR_SIZE) {
+			sector += (offset / SECTOR_SIZE);
+			offset %= SECTOR_SIZE;
+		}
+		int size;
+		if (offset + bytes > SECTOR_SIZE) { // Fill sector
+			size = SECTOR_SIZE - offset;
+			_write(ata, ans, size, sector, offset);
+			sector++;
+			offset = 0;
+			bytes -= size;
+			ans += size;
+		} else { // The remaining msg fits in the actual sector
+			size = bytes;
+			_write(ata, ans, size, sector, offset);
+			offset += size;
+			bytes = 0;
+			ans += size;
+		}
+	}
+	mt_sti();
+}
+
+void _write(int ata, char * msg, int bytes, unsigned short sector, int offset) {
+	char tmp[SECTOR_SIZE];
+	int i;
+	// Read actual sector because ATA always writes a complete sector!
+	// Don't overwrite previous values!
+	_read(ata, tmp, sector, 0, SECTOR_SIZE);
+	for (i = 0; i < bytes; i++) {
+		tmp[offset + i] = msg[i];
+	}
+	sendComm(ata, LBA_WRITE, sector);
+	// Write updated sector
+	for (i = 0; i < SECTOR_SIZE; i += 2) {
+		writeDataToRegister(ata, tmp[i + 1], tmp[i]);
+	}
+}
+
+void writeDataToRegister(int ata, char upper, char lower) {
+	mt_cli();
+	unsigned short out;
+	// Wait for driver's ready signal.
+	while (!(inw(ata + WIN_REG7) & BIT(3)))
+		;
+	out = upper << 8 | (lower & 0xff);
+	outw(ata + WIN_REG0, out);
+	mt_sti();
+}
+
+unsigned short getDataRegister(int ata) {
+	mt_cli();
+	unsigned short ans;
+	// Wait for driver's ready signal.
+	while (!(inw(ata + WIN_REG7) & BIT(3))) {
+		printk("Waiting for disk to be ready...\n");
+	}
+	ans = inw(ata + WIN_REG0);
+	mt_sti();
+	return ans;
+}
+
+unsigned short getErrorRegister(int ata) {
+	unsigned short rta = inb(ata + WIN_REG1) & 0x00000FFFF;
+	return rta;
+}
+
+void sendComm(int ata, int rdwr, unsigned short sector) {
+	outb(ata + WIN_REG1, 0);
+	outb(ata + WIN_REG2, 1); // Set count register sector in 1
+
+	outb(ata + WIN_REG3, (unsigned char) sector); // LBA low
+	outb(ata + WIN_REG4, (unsigned char) (sector >> 8)); // LBA mid
+	outb(ata + WIN_REG5, (unsigned char) (sector >> 16)); // LBA high
+	outb(ata + WIN_REG6, 0xE0 | (ata << 4) | ((sector >> 24) & 0x0F)); // Set LBA bit in 1 and the rest in 0
+	// Set command
+	outb(ata + WIN_REG7, rdwr);
+}
+
+unsigned short ata_getStatusRegister(int ata) {
+	unsigned short rta;
+	rta = inb(ata + WIN_REG7) & 0x00000FFFF;
+	return rta;
+}
+
+void identifyDevice(int ata) {
+	outb(ata + WIN_REG6, 0);
+	outb(ata + WIN_REG7, WIN_IDENTIFY);
+}
+
+/*Checks disk features*/
+void ata_checkDrive(int ata) {
+	printk("Identificando dispositivo...\n ");
+	switch (ata) {
+	case ATA0:
+		printk("ATA0...\n");
+		break;
+	case ATA1:
+		printk("ATA1...\n");
+		break;
+	}
+	printk("\n");
+	identifyDevice(ata);
+	unsigned short data = 0;
+	int i;
+	for (i = 0; i < 255; i++) {
+		data = getDataRegister(ata);
+		switch (i) {
+		case 0:
+			//printk("Data returned (%d): %d\n", i,data);
+			IS_REMOVABLE(data);
+			IS_ATA_DRIVE(data);
+			break;
+		case 49:
+			DMA_SUP(data);
+			LBA_SUP(data);
+			break;
+		case 83:
+			DMA_QUEUED_SUP(data);
+			break;
+		}
+	}
+}

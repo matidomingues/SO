f4f40db6e2e0ef04291b15700eb6c46374021898
mannias <mannias@gmail.com>
2013-06-23 23:51:14 -0300

    starting fm

--- TPE2/mtask/src/fs.c
+++ TPE2/mtask/src/fs.c
@@ -1,16 +1,79 @@
 #include "fs.h"
 
+int sectors[MAX_SECTORS]
+directory currentdir = NULL;
+
 directory createFS() {
 	directory root;
 	root.name = "root";
 	root.filecount = 0;
 	root.subdircount = 0;
-	root.parent = 0;
+	root.parent = NULL;
 	int i;
 	for (i = 0; i < 255; ++i) {
-		root.files[i] = 0;
-		root.subdirectories[i] = 0;
+		root.files[i] = NULL;
+		root.subdirectories[i] = NULL;
+	}
+	currentdir = null;
+	return root;
+}
+
+void initSectors(){
+	int i;
+	for(i=0; i<MAX_SECTORS; i++){
+		sectors[i] = 0;
+	}
+}
+
+int getFreeSector(){
+	int i;
+	for(i=0; i<MAX_SECTORS; i++){
+		if(!sectors[i]){
+			return i;
+		}
+	}
+	return -1;
+}
+
+void setSector(sector){
+	sectors[sector] = 1;
+}
+
+file getFileFromName(char* name){
+	int i;
+	file elem;
+	for(i=0; i< MAX_FILES; i++){
+		elem = currentdir.files[i]
+		if(elem != NULL){
+			if(strcmp(elem.name, name) == 0){
+				return elem;
+			}
+		}
+	}
+	return NULL;
+}
+
+file* openFile(char* name){
+	file elem = getFileFromName(name);
+	if(elem == NULL){
+		return -1;
+	}else{
+		return &elem;
 	}
 
-	return root;
+}
+
+void ls(){
+	int i;
+	printf("Directory: %s\n", currentdir.name);
+	for(i=0; i<MAX_DIRECTORIES; i++){
+		if(currentdir.subdirectories[i] != NULL){
+			printf("%s\n", currentdir.subdirectories[i].name);
+		}
+	}
+	for(i=0; i<MAX_FILES; i++){
+		if(currentdir.files[i] != NULL){
+			printf("%s\n", currentdir.files[i].name);
+		}
+	}
 }
--- TPE2/mtask/src/fs.h
+++ TPE2/mtask/src/fs.h
@@ -1,6 +1,11 @@
 #ifndef FS_H
 #define FS_H
 
+#define MAX_SECTORS 15000
+#define SECTOR_SIZE 512
+#define MAX_FILES 100
+#define MAX_DIRECTORIES 100
+
 typedef struct file {
 	char name[128];
 	int disksector;
@@ -12,7 +17,7 @@ typedef struct directory {
 	int filecount;
 	int subdircount;
 	struct directory parent;
-	struct file files[255];
+	struct file files[MAX_FILES];
 	struct directory subdirectories[255];
 } directory;
 
